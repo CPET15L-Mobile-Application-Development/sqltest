0f004aa720d2f4889a6b0eb221fd83a9dd7def87
Wataru <Wataru@DESKTOP-C85T8EI>
2024-05-31 18:55:01 +0800

    Add project files.

---
+++ sqltest/sqltest/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.IO;
+
+
+namespace sqltest
+{
+    public partial class App : Application
+    {
+
+        private static SQLiteHelper db;
+
+        public static SQLiteHelper MyDatabase
+        {
+            get
+            {
+                if (db == null)
+                {
+                    db = new SQLiteHelper(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "MyStore.db3"));
+                }
+                return db;
+            }
+        }
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ sqltest/sqltest/View/EmployeeDetail.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace sqltest.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EmployeeDetail : ContentPage
+    {
+        public EmployeeDetail()
+        {
+            InitializeComponent();
+        }
+        Model.EmployeeModel _employee;
+        public EmployeeDetail(Model.EmployeeModel employee)
+        {
+            InitializeComponent();
+            Title = "Edit Information";
+            _employee = employee;
+            nameEntry.Text = _employee.Name;
+            addressEntry.Text = _employee.Address;
+            nameEntry.Focus();
+        }
+
+        async void Button_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(nameEntry.Text) || string.IsNullOrWhiteSpace(addressEntry.Text))
+            {
+                await DisplayAlert("Invalid", "Blank or Whitespace value is Invalid!", "OK");
+            }else if (_employee != null)
+            {
+                UpdateEmployee();
+            }
+            else
+                AddNewEmployee();
+
+
+
+        }
+        async void AddNewEmployee()
+        {
+            await App.MyDatabase.CreateEmployee(new Model.EmployeeModel
+            {
+                Name = nameEntry.Text,
+                Address = addressEntry.Text
+            });
+            await Navigation.PopAsync();
+        }
+        async void UpdateEmployee()
+        {
+            _employee.Name = nameEntry.Text;
+            _employee.Address = addressEntry.Text;
+            await App.MyDatabase.UpdateEmployee(_employee);
+            await Navigation.PopAsync();
+        }
+    }
+}
